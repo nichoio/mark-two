e6ee23b7fca5be50521ff8cc87d8ea355f409338
Nicholas <nicholaskappel@prezap.de>
2018-07-30 14:11:01 +0200

    add templates for lists. add view for keywords. adapt routes to these changes

--- server.js
+++ server.js
@@ -55,19 +55,32 @@ app.post('/create', function(req, res){
         };
 
         db.addTask(json); //keep track of this contract in DB
-        res.redirect('/tasks/' + values[1]); //show tasks of owner afterwards
+        res.redirect('/tasks/owner/' + values[1]); //show tasks of owner afterwards
     });
 });
 
-app.get('/tasks/:owner', function(req, res){
+app.get('/tasks/owner/:owner', function(req, res){
     db.getTasksByOwner(req.params.owner)
     .then(function(values){
         for (let i = 0; i < values.length; i++) {
             // YYYY-MM-DD HH:MM:SS -> YYYY-MM-DD
             values[i].created_utc = values[i].created_utc.split(" ")[0];
         }
+
         var ownerShort = req.params.owner.substring(0, 8) + '...'; 
-        res.render('tasks.html', {tasks: values, ownerShort: ownerShort});
+        res.render('tasksowner.html', {tasks: values, ownerShort: ownerShort});
+    });
+});
+
+app.get('/tasks/keyword/:keyword', function(req, res){
+    db.getTasksByKeyword(req.params.keyword)
+    .then(function(values){
+        for (let i = 0; i < values.length; i++) {
+            // YYYY-MM-DD HH:MM:SS -> YYYY-MM-DD
+            values[i].created_utc = values[i].created_utc.split(" ")[0];
+        }
+
+        res.render('taskskeyword.html', {tasks: values, keyword: req.params.keyword});
     });
 });
 
