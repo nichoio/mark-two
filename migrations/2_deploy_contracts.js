0dd383349fa69a3b4fb0e6e3e2576274a53e5dfa
Nicholas <nicholaskappel@prezap.de>
2018-07-31 18:36:44 +0200

    first tests for Task.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
-// var HelloWorld = artifacts.require("HelloWorld");
+var Task = artifacts.require("Task");
 
-// module.exports = function(deployer) {
-//   deployer.deploy(HelloWorld);
-// };
+module.exports = function(deployer) {
+    deployer.deploy(
+        Task,
+        "0x12345d9cbe3a42899919fbd098d18f79bcf9a071",
+        "What is 2+2?",
+        "testkey",
+        "42"
+    );
+};
