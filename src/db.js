551bdeae60b582f7e517adc1d24e6b54e45e9e96
Nicholas <nicholaskappel@prezap.de>
2018-08-01 23:36:18 +0200

    refactoring db.js and adding tests

--- src/db.js
+++ src/db.js
@@ -9,31 +9,35 @@ class DB{
     }
 
     addTask(json) {
-        con = json.contract;
-        que = json.question;
-        own = json.owner;
-        cor = json.corrector;
-        key = json.keyword;
-        max = json.maxScore;
+        var con = json.contract;
+        var que = json.question;
+        var own = json.owner;
+        var cor = json.corrector;
+        var key = json.keyword;
+        var max = json.maxScore;
 
-        //check if Tasks table already has an entry under this contract address
-        db.all("SELECT * FROM Tasks WHERE contract LIKE '" + con + "'", function(err, rows) {
-            if (rows.length == 0){ //save this contract for the first time
-                db.run(
-                    "INSERT INTO Tasks (contract, question, owner, corrector, keyword, maxscore)" +
-                    "VALUES ('" + con + "', '" + que + "', '" + own + "', '" +
-                    cor + "', '" + key + "', " + max + ");");
-                console.log("Save new address to Tasks: " + con);
-            }
-            else if (rows.length == 1) return;  // don't save since it's already there
-            else { // should never happen hence let's exit
-                console.error(
-                    "The following Task address is saved multiple times " + 
-                    "(" + rows.length.toString() + " times) in table Tasks: " +
-                    con);
-                process.exit(1); // terminate server
-            }
-        });
+        return new Promise(function (resolve, reject) {
+            //check if Tasks table already has an entry under this contract address
+            this.db.all("SELECT * FROM Tasks WHERE contract LIKE '" + con + "'", function(err, rows) {
+                if (rows.length == 0){ //save this contract for the first time
+                    this.db.run(
+                        "INSERT INTO Tasks (contract, question, owner, corrector, keyword, maxscore)" +
+                        "VALUES ('" + con + "', '" + que + "', '" + own + "', '" +
+                        cor + "', '" + key + "', " + max + ");", function() {
+                        console.log("Save new address to Tasks: " + con);
+                        resolve();
+                    });
+                }
+                else if (rows.length == 1) resolve();  // don't save since it's already there
+                else { // should never happen hence let's exit
+                    console.error(
+                        "The following Task address is saved multiple times " + 
+                        "(" + rows.length.toString() + " times) in table Tasks: " +
+                        con);
+                    process.exit(1); // terminate server
+                }
+            }.bind(this));
+        }.bind(this));
     }
 
     getTasksByOwner(address) {
@@ -80,10 +84,10 @@ class DB{
         .then(function(rows){
             //check if Testees table already has an answer saved for this task and this testee
             if (rows.length == 0){ //save this answer at the first time
-                return db.run("INSERT INTO Testees (contract, testee, answer) " +
-                    "VALUES ('" + contract + "', '" + testee + "', '" + String(answer) + "');");
+                return this.db.run("INSERT INTO Testees (contract, testee, answer) " +
+                    "VALUES ('" + contract + "', '" + testee + "', '" + answer + "');");
                 console.log("Save new answer for Task: " + contract);
-                //TODO: only show this after transaction is confirmed -> serialize?!
+                //TODO: only show this after transaction is confirmed
             }
             else if (rows.length == 1) return;  // don't save since it's already there
             else { // should never happen hence let's exit
@@ -93,7 +97,7 @@ class DB{
                     contract + ", " + testee);
                 process.exit(1); // terminate server
             }
-        });    
+        }.bind(this));    
     }
 }
 
--- test/src/dbReadTest.js
+++ test/src/dbReadTest.js
@@ -8,7 +8,7 @@ const dbModule = require('./../../src/db');
 
 var db = new dbModule.DB(dbTestPath);
 
-describe('db', function(){
+describe('dbRead', function(){
     var dbTest;
 
     before(function() {
@@ -31,6 +31,9 @@ describe('db', function(){
                     "corrector TEXT NOT NULL, keyword TEXT NOT NULL, maxscore INTEGER NOT NULL, " +
                     "created_utc TEXT DEFAULT CURRENT_TIMESTAMP);");
                 dbTest.run(
+                    "CREATE TABLE Testees (contract TEXT NOT NULL, testee TEXT NOT NULL, " +
+                    "answer TEXT NOT NULL, score INTEGER);");
+                dbTest.run(
                     "INSERT INTO Tasks (contract, question, owner, corrector, keyword, maxscore)" +
                     "VALUES ('0x992340b63317E301a7B3828Cee73dE7af08c6543', " +
                     "'What is 2+2?', '0xd0b98B0a7cCa6c5F7099E79DdD79Cfee0f4c3121', " +
@@ -41,17 +44,31 @@ describe('db', function(){
                     "VALUES ('0xcf39DFd741a02634dBCD5067b5F60272E39418E7', " +
                     "'What is transparent and smells like worms?', " + 
                     "'0xd0b98B0a7cCa6c5F7099E79DdD79Cfee0f4c3121', " +
-                    "'0xE51D0Aed7B6125f66c735238C6c7063B7e4A2dCf', 'testkeyword', 8);", function(){
-                    resolve();
+                    "'0xE51D0Aed7B6125f66c735238C6c7063B7e4A2dCf', 'testkeyword', 8);");
+                dbTest.run("INSERT INTO Testees (contract, testee, answer) " +
+                    "VALUES ('0x992340b63317E301a7B3828Cee73dE7af08c6543', " +
+                    "'0xfFc8c5b844E2a0DB895D6E29Fd757Df0285AEEF6', 'myAnswer');");
+                dbTest.run("INSERT INTO Testees (contract, testee, answer) " +
+                    "VALUES ('0x992340b63317E301a7B3828Cee73dE7af08c6543', " +
+                    "0x20F9384e132cB839FE970fBA7a0601D0C21fa9fE, 'myDifferentAnswer');", function(){
+                    resolve(); //must go here, otherwise won't work
                 });
             });
         });
     });
 
-    it("getTasksByOwner", function(done) {
-        db.getTasksByOwner("0xd0b98B0a7cCa6c5F7099E79DdD79Cfee0f4c3121")
+    it('getTasksByOwner', function(done) {
+        db.getTasksByOwner('0xd0b98B0a7cCa6c5F7099E79DdD79Cfee0f4c3121')
         .then(function(values){
-            assert.equal(values.length, 2, "It's not 2");
+            assert.equal(values.length, 2, 'It\'s not 2');
+            done();
+        });
+    });
+
+    it('getTasksByKeyword', function(done) {
+        db.getTasksByKeyword('testkeyword')
+        .then(function(values){
+            assert.equal(values.length, 2, 'It\'s not 2"');
             done();
         });
     });
@@ -69,4 +86,3 @@ describe('db', function(){
         });
     });
 });
-
