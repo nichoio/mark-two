934e298c2bfd435c261a6d7b6137268a95a563f9
Nicholas <nicholaskappel@prezap.de>
2018-08-01 23:36:18 +0200

    refactoring db.js and adding tests

---
+++ test/src/dbWriteTest.js
@@ -0,0 +1,110 @@
+const spawn = require('child_process').spawn;
+
+const assert = require('chai').assert;
+const sqlite3 = require('sqlite3');
+
+const dbTestPath = './test/src/test2_db.sqlt';
+const dbModule = require('./../../src/db');
+
+var db = new dbModule.DB(dbTestPath);
+
+describe('dbWrite', function(){
+    var dbTest;
+
+    before(function() {
+        return new Promise(function (resolve) {
+            const sqlite3CMD = spawn('sqlite3', [dbTestPath]);
+
+            sqlite3CMD.stdout.on('data', (data) => {
+              console.log(`stdout: ${data}`);
+            });
+
+            sqlite3CMD.stderr.on('data', (data) => {
+              console.log(`stderr: ${data}`);
+            });
+
+            dbTest = new sqlite3.Database(dbTestPath);
+
+            task1 = {
+                "contract": "0xfE1C5527AdCEc99d078Eb418B7B43d4e81713B83",
+                "question": "What is 2+2?", 
+                "owner": "0x7E271C47FFd7A52caCa98B4D0483448693c0b023",
+                "corrector": "0xE807b266912cd9f117a42748A158328d677Ed789",
+                "keyword": "testkeyword",
+                "maxScore": "10"};
+
+            task2 = {
+                "contract": "0xc8CdEE3335EB037aAb43042177C3D6AA34FF64aF",
+                "question": "What is 2+2?", 
+                "owner": "0x7E271C47FFd7A52caCa98B4D0483448693c0b023",
+                "corrector": "0xBEfDb4885A572BA3d3482E016A76311c0a58a026",
+                "keyword": "testkeyword",
+                "maxScore": "10"};
+
+            dbTest.serialize(function() {
+                dbTest.run(
+                    "CREATE TABLE Testees (contract TEXT NOT NULL, testee TEXT NOT NULL, " +
+                    "answer TEXT NOT NULL, score INTEGER);", function(){
+                    db.addTaskAnswer(
+                            '0xe3c68818cD85D16106ac92fd7763a6B56eB0E60E',
+                            '0xD819B3f5d3453c7cEaa9f25004ceb767dCcc0b40', 'myAnswer');
+                    db.addTaskAnswer(
+                            '0xe3c68818cD85D16106ac92fd7763a6B56eB0E60E',
+                            '0xC4Dfa83bb50681576a86078B4C772BD40eA586D8', 'myDifferentAnswer');
+                });
+                dbTest.run(
+                    "CREATE TABLE Tasks (contract TEXT NOT NULL, owner TEXT NOT NULL, "+
+                    "question TEXT NOT NULL, corrector TEXT NOT NULL, " +
+                    "keyword TEXT NOT NULL, maxscore INTEGER NOT NULL, " +
+                    "created_utc TEXT DEFAULT CURRENT_TIMESTAMP);", function(){
+                    db.addTask(task1)
+                    .then(function () {
+                        db.addTask(task2)
+                        .then(function () {
+                            resolve();
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('addTask', function(done) {
+        dbTest.all(
+            "SELECT * FROM Tasks;", function(err, rows) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            } else {
+                assert.equal(rows.length, 2, 'It\'s not 2"');
+                done();
+            }
+        });
+    });
+
+    it('addTaskAnswer', function(done) {
+        dbTest.all(
+            "SELECT * FROM Testees;", function(err, rows) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            } else {
+                assert.equal(rows.length, 2, 'It\'s not 2"');
+                done();
+            }
+        });
+    });
+
+    after(function() {
+        dbTest.close();
+        const rm = spawn('rm', [dbTestPath]);
+
+        rm.stdout.on('data', (data) => {
+          console.log(`stdout: ${data}`);
+        });
+
+        rm.stderr.on('data', (data) => {
+          console.log(`stderr: ${data}`);
+        });
+    });
+});
