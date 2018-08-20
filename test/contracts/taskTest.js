930c21044468b0a218b9db78b62a5f8db5286961
Nicholas <nicholaskappel@prezap.de>
2018-08-20 15:26:13 +0200

    fix existing tests and add new ones

--- test/contracts/taskTest.js
+++ test/contracts/taskTest.js
@@ -1,13 +1,36 @@
-var Task = artifacts.require("Task");
+var Task = artifacts.require('Task');
 
-//TODO: increase test coverage for Task
+//inspired by:
+//https://github.com/MichalZalecki/tdd-solidity-intro/blob/master/test/FundingTest.js
+contract('Task', accounts => {
+    const [account1, account2, account3] = accounts;
+    let task;
 
-contract('Task', function() {
-    it("Init task", function() {
-        return Task.deployed().then(function(instance) {
-            return instance.question.call();
-        }).then(function(question) {
-            assert.equal(question, "What is 2+2?", "It's not 2+2");
+    beforeEach(async () => {
+        task = await Task.deployed();
+    });
+
+    it('call question', function() {
+        return task.question.call()
+        .then(function(question) {
+            assert.equal(question, 'What is 2+2?', 'It\'s not "2+2"');
         });
     });
+
+    it('call keyword', function() {
+        return task.keyword.call()
+        .then(function(keyword) {
+            assert.equal(keyword, 'testkey', 'It\'s not "testkey"');
+        });
+    });
+
+    it("answer", async () => {
+        await task.solve('myAnswer', { from: account1});
+        return task.answers.call(account1)
+        .then(function(answer) {
+            assert.equal(answer, 'myAnswer', 'It\'s not "myAnswer"');
+        });
+    });
+
+    //More tests for task correcting and more complex cases could be added here...
 });
