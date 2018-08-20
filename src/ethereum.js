3a382eadb994b72258727dab22b8381976fde0aa
Nicholas <nicholaskappel@prezap.de>
2018-08-20 15:25:47 +0200

    refactor ethereum.js to class

--- src/ethereum.js
+++ src/ethereum.js
@@ -3,48 +3,49 @@ const Web3 = require('web3');
 const taskAbi = require('./eth/TaskABI.json');
 const providers = require('./../secrets/providers.json');
 
-var web3 = new Web3(new Web3.providers.HttpProvider(providers.ropsten));
+class Eth{
+    constructor(provider) {
+        this.web3 = new Web3(new Web3.providers.HttpProvider(provider));
+    }
 
-function getTaskByTransaction(hash) {
-    return new Promise(function (resolve, reject) {
-        web3.eth.getTransactionReceipt(hash)
-        .then(function(data){
-            if (data) {
-                //return destination of transaction, i.e. the task contract
-                resolve(data.contractAddress);
-            }
-            else{
-                reject(new TypeError('Receipt is null'));
-            }
-        });
-    });
-}
+    getTaskByTransaction(hash) {
+        return new Promise(function (resolve, reject) {
+            this.web3.eth.getTransactionReceipt(hash)
+            .then(function(data){
+                if (data) {
+                    //return destination of transaction, i.e. the task contract
+                    resolve(data.contractAddress);
+                }
+                else{
+                    reject(new TypeError('Receipt is null'));
+                }
+            });
+        }.bind(this)); 
+    }
 
-function getTaskData(address) {
-    var task = new web3.eth.Contract(taskAbi, address);
+    getTaskData(address) {
+        var task = new this.web3.eth.Contract(taskAbi, address);
 
-    var p1 = task.methods.question().call();
-    var p2 = task.methods.owner().call();
-    var p3 = task.methods.corrector().call();
-    var p4 = task.methods.keyword().call();
-    var p5 = task.methods.maxScore().call();
+        var p1 = task.methods.question().call();
+        var p2 = task.methods.owner().call();
+        var p3 = task.methods.corrector().call();
+        var p4 = task.methods.keyword().call();
+        var p5 = task.methods.maxScore().call();
 
-    return Promise.all([p1, p2, p3, p4, p5]);
-}
+        return Promise.all([p1, p2, p3, p4, p5]);
+    }
 
-function getAnswer(address, testee) {
-    var task = new web3.eth.Contract(taskAbi, address);
-    return task.methods.answers(testee).call();
-}
+    getAnswer(address, testee) {
+        var task = new this.web3.eth.Contract(taskAbi, address);
+        return task.methods.answers(testee).call();
+    }
 
-function getAnswerScore(address, testee) {
-    var task = new web3.eth.Contract(taskAbi, address);
-    return task.methods.scores(testee).call();
+    getAnswerScore(address, testee) {
+        var task = new this.web3.eth.Contract(taskAbi, address);
+        return task.methods.scores(testee).call();
+    }
 }
 
 module.exports = {
-    getTaskByTransaction: getTaskByTransaction,
-    getTaskData: getTaskData,
-    getAnswer: getAnswer,
-    getAnswerScore: getAnswerScore
+    Eth: Eth
 };
